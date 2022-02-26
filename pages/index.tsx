08972e8b182731d418f569b7576d41c348fb2dea
JY, Park <bleusoire@gmail.com>
2022-02-26 14:55:08 +0900

    updated url for og:image tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,23 +3,21 @@ import React from 'react';
 import Head from 'next/head';
 
 function Home() {
-
   return (
     <>
       <Head>
         <title>Newv.dev</title>
-        <meta
-          name='description'
-          content='Newv is service of new version detection and notifier for developer.'
-        />
+        <meta name='description' content='Newv is service of new version detection and notifier for developer.' />
         <meta charSet='utf-8' />
 
         <meta property='og:title' content='Newv' />
         <meta property='og:url' content='https://www.newv.dev' />
         <meta property='og:type' content='website' />
-        <meta property='og:image' content='og_image.png' />
-        <meta property='og:description'
-          content='Newv is service of new version detection and notifier for developer.' />
+        <meta property='og:image' content='https://storage.googleapis.com/pub_bucket_1/og_image.png' />
+        <meta
+          property='og:description'
+          content='Newv is service of new version detection and notifier for developer.'
+        />
 
         <link rel='icon' href='/favicon.ico' />
       </Head>
@@ -28,8 +26,9 @@ function Home() {
           <Heading size='xl'>What is Newv?</Heading>
         </Center>
         <Center mt='14px'>
-          <Heading size='md' pl='10px' pr='10px'>Newv is service of new version detection and notifier for
-            developer.</Heading>
+          <Heading size='md' pl='10px' pr='10px'>
+            Newv is service of new version detection and notifier for developer.
+          </Heading>
         </Center>
       </Box>
     </>
